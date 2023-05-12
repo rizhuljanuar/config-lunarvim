880e8e845df0f98d2fd2215a6d06ecdb4fbf2918
Rizhul Januar <rayjam66@gmail.com>
2023-05-12 08:21:29 +0700

    first commit

---
+++ config-backup.lua
@@ -0,0 +1,383 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general zhul
+    reload('config.general');
+-- end general zhul
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = false
+lvim.colorscheme = "lunar"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- add by zhul
+lvim.keys.visual_mode["J"] = ":move '>+1<CR>gv-gv"
+lvim.keys.visual_mode["K"] = ":move '<-2<CR>gv-gv"
+lvim.keys.visual_mode["<A-j>"] = ":move '>+1<CR>gv-gv"
+lvim.keys.visual_mode["<A-Down>"] = ":move '>+1<CR>gv-gv"
+lvim.keys.visual_mode["<A-k>"] = ":move '<-2<CR>gv-gv"
+lvim.keys.visual_mode["<A-Up>"] = ":move '<-2<CR>gv-gv"
+lvim.keys.visual_mode["<S-Down>"] = ":'<,'>t'><cr>"
+
+lvim.keys.insert_mode["<S-Down>"] = "<cmd>t.<cr>"
+lvim.keys.insert_mode["<S-Up>"] = "<cmd>t -1<cr>"
+lvim.keys.insert_mode["<M-Down>"] = "<cmd>m+<cr>"
+lvim.keys.insert_mode["<M-Up>"] = "<cmd>m-2<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<cmd>w<cr>"
+lvim.keys.insert_mode["<C-l>"] = "<cmd>LiveServer start<cr>"
+lvim.keys.normal_mode["<S-Down>"] = "<cmd>t.<cr>"
+lvim.keys.normal_mode["<S-Up>"] = "<cmd>t -1<cr>"
+lvim.keys.normal_mode["<M-J>"] = "<cmd>t.<cr>"
+lvim.keys.normal_mode["<M-K>"] = "<cmd>t -1<cr>"
+lvim.keys.normal_mode["<M-Down>"] = "<cmd>m+<cr>"
+lvim.keys.normal_mode["<M-Up>"] = "<cmd>m-2<cr>"
+lvim.keys.normal_mode["<M-j>"] = "<cmd>m+<cr>"
+lvim.keys.normal_mode["<M-k>"] = "<cmd>m-2<cr>"
+lvim.keys.normal_mode["<C-s>"] = "<cmd>w<cr>"
+lvim.keys.normal_mode["q"] = "<cmd>q<cr>"
+-- end by zhul
+
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- unmap a default keymapping
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+
+-- Change theme settings
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "java",
+    "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+-- lvim.lsp.installer.setup.ensure_installed = {
+--     "sumneko_lua",
+--     "jsonls",
+-- }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+
+-- Additional Plugins
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- reload('lua.indent')
+lvim.autocommands = {
+    {
+        "BufEnter", -- see `:h autocmd-events`
+        { -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
+            pattern = { "*.php", "*.go" }, -- see `:h autocmd-events`
+            command = "setlocal tabstop=4 shiftwidth=4",
+        }
+    }
+}
+
+lvim.builtin.alpha.dashboard.section.header.val = {
+    [[                                                                          ]],
+    [[   $$$$$\                     $$\                 $$\           $$\       ]],
+    [[   \__$$ |                    $$ |                \__|          $$ |      ]],
+    [[      $$ | $$$$$$\   $$$$$$$\ $$ |  $$\  $$$$$$\  $$\ $$$$$$$$\ $$$$$$$\  ]],
+    [[      $$ | \____$$\ $$  _____|$$ | $$  |$$  __$$\ $$ |\____$$  |$$  __$$\ ]],
+    [[$$\   $$ | $$$$$$$ |$$ /      $$$$$$  / $$ |  \__|$$ |  $$$$ _/ $$ |  $$ |]],
+    [[$$ |  $$ |$$  __$$ |$$ |      $$  _$$<  $$ |      $$ | $$  _/   $$ |  $$ |]],
+    [[\$$$$$$  |\$$$$$$$ |\$$$$$$$\ $$ | \$$\ $$ |      $$ |$$$$$$$$\ $$ |  $$ |]],
+    [[ \______/  \_______| \_______|\__|  \__|\__|      \__|\________|\__|  \__|]],
+}
+local function footer()
+    return "Jackrizh"
+end
+
+lvim.builtin.alpha.dashboard.section.footer.val = footer()
+
+lvim.plugins = {
+    { "manzeloth/live-server" },
+    { "APZelos/blamer.nvim" },
+    { "wuelnerdotexe/vim-astro" },
+    { "yaegassy/coc-blade" },
+    { "jwalton512/vim-blade" },
+    { "mg979/vim-visual-multi" },
+    { "alvan/vim-closetag" },
+    { "leafOfTree/vim-vue-plugin" },
+    { "yardnsm/vim-import-cost" },
+    { "CRAG666/code_runner.nvim",
+        requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("config.coderunner")
+		end,
+    },
+    { "NvChad/nvim-colorizer.lua",
+        config = function ()
+            require("config.colorizer")
+        end
+    }
+}
+
+require("lvim.lsp.manager").setup("tailwindcss", {
+  filetypes = { "html", "vue", "typescriptreact", "javascriptreact" }
+})
+
+require("lvim.lsp.manager").setup("emmet_ls", {
+  filetypes = { "html", "vue" ,"typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "eruby" }
+})
+
+lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope find_files hidden=true no_ignore=true<CR>", "Find file" }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "typescript", "typescriptreact" },
+  },
+}
+
+-- lvim.builtin.which_key.mappings["sF"] = { "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", "Find File Everywhere" }
+-- lvim.builtin.which_key.mappings["sT"] = {
+-- function()
+-- require("telescope.builtin").live_grep {
+-- additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+-- }
+-- end,
+-- "Text Everywhere",
+-- }
+
+-- coderunner
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Run",
+	s = {
+		'<cmd>autocmd bufwritepost [^_]*.sass,[^_]*.scss  silent exec "!sass %:p %:r.css"<CR>',
+		"Auto Compile Sass",
+	},
+	r = { "<cmd>RunCode<CR>", "Run Code" },
+	f = { "<cmd>RunFile<CR>", "Run File" },
+	p = { "<cmd>RunProject<CR>", "Run Project" },
+	g = { "<cmd>ToggleTerm size=70 direction=vertical<cr>gradle run<cr>", "Run Gradle" },
+	m = {
+		"<cmd>ToggleTerm size=70 direction=vertical<cr>mvn exec:java -Dexec.mainClass=com.pojokcode.App<cr>",
+		"Run MVN",
+	},
+}
+
+local check_backspace = function()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+lvim.builtin.cmp.mapping = {
+	["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+	["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+	["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+	["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+	["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+	["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+	["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+	["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+	["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+	["<C-y>"] = cmp.config.disable,
+	["<C-e>"] = cmp.mapping({
+		i = cmp.mapping.abort(),
+		c = cmp.mapping.close(),
+	}),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
+	["<Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		elseif luasnip.expandable() then
+			luasnip.expand()
+		elseif luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		elseif check_backspace() then
+			fallback()
+		else
+			fallback()
+		end
+	end, {
+		"i",
+		"s",
+	}),
+
+	["<S-Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+	end, {
+		"i",
+		"s",
+	}),
+}
+
+-- lsp installer
+lvim.lsp.installer.setup.ensure_installed = {
+	"sumneko_lua",
+	"jsonls",
+	"html",
+	"cssls",
+	"emmet_ls",
+	"tsserver",
+	"intelephense",
+	"tailwindcss",
+}
+
+require("lvim.lsp.manager").setup("emmet_ls")
+require("lvim.lsp.manager").setup("tailwindcss")
+require("lvim.lsp.manager").setup("intelephense")
+
+
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua", filetype = { "lua" } },
+	{ command = "prettier" },
+	{ command = "blade_formatter", filetype = { "php", "blade", "blade.php" } },
+})
